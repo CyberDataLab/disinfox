b84cafb186f318e26ad468cbfa7e27b6a5029534
Felipe Sanchez <felipe.sanchezg@um.es>
2024-12-03 00:57:33 +0100

    better env management

--- public-api/app.py
+++ public-api/app.py
@@ -5,14 +5,14 @@ import requests
 app = Flask(__name__)
 
 # Getting the environment variables
-DISINFOX_BACKEND_URL = os.environ.get('DISINFOX_BACKEND_URL')
+BACKEND_ROOT = f"http://{os.environ['BACKEND_HOST']}:{os.environ['BACKEND_PORT']}/"
 
-app.logger.info("Checking connection with DISINFOX backend: " + DISINFOX_BACKEND_URL)
+app.logger.info("Checking connection with DISINFOX backend: " + BACKEND_ROOT)
 try:
-    response = requests.get(DISINFOX_BACKEND_URL)
+    response = requests.get(BACKEND_ROOT)
     response.raise_for_status()
 except:
-    app.logger.error("The backend" + DISINFOX_BACKEND_URL + " is not reachable")
+    app.logger.error("The backend" + BACKEND_ROOT + " is not reachable")
     exit(1)
 app.logger.info("[OK :)] Connection with DISINFOX backend established")
 
@@ -28,7 +28,7 @@ def check_auth():
         return jsonify({'message': 'Please provide an Authorization header with a valid token'}), 401
     token = request.headers['Authorization']
     app.logger.info("Checking token: " + token)
-    response = requests.post(DISINFOX_BACKEND_URL + 'check-api-key', data={'api_key': token})
+    response = requests.post(BACKEND_ROOT + 'check-api-key', data={'api_key': token})
     if response.status_code == 200:
         return jsonify({'message': 'The token is valid'})
     else:
@@ -41,12 +41,12 @@ def incidents():
     if 'Authorization' not in request.headers:
         return jsonify({'message': 'Please provide an Authorization header with a valid token'})
     token = request.headers['Authorization']
-    response = requests.post(DISINFOX_BACKEND_URL + 'check-api-key', data={'api_key': token})
+    response = requests.post(BACKEND_ROOT + 'check-api-key', data={'api_key': token})
     newer_than = request.args.get('newer_than', default=None)
     if not newer_than:
         return jsonify({'message': 'Please provide a newer_than parameter, e.g. /incidents?newer_than=2024-11-30T01:35:21.128381Z'})
     app.logger.info("Getting incidents from DISINFOX backend newer than: " + newer_than)
-    response = requests.get(DISINFOX_BACKEND_URL + 'incidents', params={'newer_than': newer_than})
+    response = requests.get(BACKEND_ROOT + 'incidents', params={'newer_than': newer_than})
     app.logger.info("Response from DISINFOX backend: " + response.text)
     return jsonify(response.json())
 
